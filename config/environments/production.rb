19eaf6aa2af097fdb1a306a1c047f90a649f5761
bdt17 <brett.thomas29.97@gmail.com>
2025-12-07 21:05:44 -0700

    fix smtp production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,3 +9,14 @@ config.action_mailer.smtp_settings = {
   enable_starttls_auto: true
 }
 config.action_mailer.default_url_options = { host: 'helpdesk-tickets-yfpr.onrender.com' }
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  user_name: 'brett.thomas29.97@gmail.com',
+  password: ENV['SMTP_PASSWORD'],
+  authentication: 'login',
+  enable_starttls_auto: true
+}
+config.action_mailer.default_url_options = { host: 'helpdesk-tickets-yfpr.onrender.com' }
