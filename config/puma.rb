27ea09af40faafbce8fac5a992b5330d822f3ac5
bdt17 <brett.thomas29.97@gmail.com>
2025-12-09 15:22:25 -0700

    FIX: Puma 0.0.0.0 + Render DATABASE_URL

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,6 @@
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
-bind        "tcp://0.0.0.0:#{port}"
+port = ENV.fetch('PORT', 3000)
+bind "tcp://0.0.0.0:#{port}"
+workers 1
+threads 1, 1
+preload_app!
+environment ENV.fetch('RAILS_ENV') { 'production' }
