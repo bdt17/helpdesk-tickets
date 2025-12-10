8146775464b9c3a05d16b2c5a007c7118a00f4f1
bdt17 <brett.thomas29.97@gmail.com>
2025-12-09 19:53:55 -0700

    fix: user callback placement

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  # Devise modules...
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :set_default_role
+
+  private
+
+  def set_default_role
+    self.role ||= 'client'
+    save!
+  end
 end
