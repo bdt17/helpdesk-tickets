303d9f64fd561a83e2a423f6e1cb3b507d7f0fc0
bdt17 <brett.thomas29.97@gmail.com>
2025-12-23 20:38:04 -0700

    FIX: routes.rb DSL block

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
-root "home#index"
 Rails.application.routes.draw do
   root "home#index"
-  
-  get "login", to: "sessions#new"
-  post "session", to: "sessions#create"
-  delete "session", to: "sessions#destroy"
-  get "logout", to: "sessions#destroy"
-  
-  get "dashboard", to: "home#dashboard"
-  
-  # Tickets (uncomment after scaffold)
-  # resources :tickets
 end
