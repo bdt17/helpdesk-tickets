bf36e6e39fb8e19d93ba7b8f4aac1f28f991c500
bdt17 <brett.thomas29.97@gmail.com>
2025-12-23 19:50:42 -0700

    FIX: Empty shipments_controller

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,12 +1,2 @@
 class ShipmentsController < ApplicationController
-  before_action :set_shipment, only: %i[show edit update destroy]
-
-  def index
-    @shipments = Shipment.all
-  end
-
-  private
-    def set_shipment
-      @shipment = Shipment.find(params[:id])
-    end
 end
