2174946fc2742f061f582635e2c5e989ad253a94
bdt17 <brett.thomas29.97@gmail.com>
2026-01-13 23:08:34 -0700

    Thomas IT Helpdesk Phase 4 - Fix helper_method in concern

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -12,14 +12,4 @@ module Authentication
       redirect_to new_session_path, alert: "Please log in"
     end
   end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-  helper_method :current_user
-
-  def user_signed_in?
-    !current_user.nil?
-  end
-  helper_method :user_signed_in?
 end
