4a6ce1698ccccf981ce00230ada4ffb176634ad9
bdt17 <brett.thomas29.97@gmail.com>
2026-01-26 16:28:12 -0700

    FIX: Network DashboardController safe queries

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    @open_tickets = SopTicket.where.not(status: nil).count
     @total_tickets = SopTicket.count
-    @critical_tickets = SopTicket.where("priority >= ?", 3).count rescue 0
+    @open_tickets = SopTicket.where.not(status: nil).count rescue 0
+    @critical_tickets = SopTicket.where("priority > ?", 1).count rescue 0
   end
 end
