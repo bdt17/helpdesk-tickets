bebf1c8a4b9c0ad411abf2cd1111779ca4f70279
bdt17 <brett.thomas29.97@gmail.com>
2026-01-27 21:23:21 -0700

    FIX: Working User model for Rails 8.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,4 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  enum status: { inactive: 0, active: 1 }, _default: :active
-  enum role: { customer: 0, agent: 1, admin: 2 }, _default: :customer
-
-  def agent?
-    role.in?(%w[agent admin])
-  end
-
-  def admin?
-    role == 'admin'
-  end
-
-  def active_for_authentication?
-    super && active?
-  end
-
-  def inactive_message
-    active? ? super : :account_disabled
-  end
 end
